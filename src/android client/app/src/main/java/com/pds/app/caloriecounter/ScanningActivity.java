f6075ed270bedda6649bd0e26c2f269d22998dbc
Aurélien van Delft <avdelft@ulb.ac.be>
2015-12-16 15:22:35 +0100

    refactoring scanning activity. still need to test the scanner with a proper device

--- src/android client/app/src/main/java/com/pds/app/caloriecounter/ScanningActivity.java
+++ src/android client/app/src/main/java/com/pds/app/caloriecounter/ScanningActivity.java
@@ -3,10 +3,9 @@ package com.pds.app.caloriecounter;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.view.MenuItem;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -14,76 +13,50 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class ScanningActivity extends HomeActivity{
 
-    private IntentIntegrator integrator;
-
-    private void initScan(){
-        integrator = new IntentIntegrator(this);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
-        integrator.setPrompt("Scan a barcode");
-
-    }
+    private Button scan;
+    private TextView format, content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        v = getLayoutInflater().inflate(R.layout.activity_camera,frameLayout);
-        initScan();
-        integrator.initiateScan();
+        v = getLayoutInflater().inflate(R.layout.activity_camera, frameLayout);
 
-    }
+        scan = (Button) v.findViewById(R.id.scan_button);
+        format = (TextView) v.findViewById(R.id.formatTxt);
+        content = (TextView) v.findViewById(R.id.contentTxt);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                Log.d("MainActivity", "Cancelled scan");
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-            } else {
-                Log.d("MainActivity", "Scanned");
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+        scan.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startScan();
             }
-        } else {
-            Log.d("MainActivity", "Weird");
-            // This is important, otherwise the result will not be passed to the fragment
-            super.onActivityResult(requestCode, resultCode, data);
-        }
+        });
+
     }
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    public void startScan() {
+        IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+        scanIntegrator.initiateScan();
+    }
 
-        if (id == R.id.nav_home) {
-            Intent homeactivity = new Intent(ScanningActivity.this, Home.class);
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
 
-            startActivity(homeactivity);
-        } else if (id == R.id.nav_data) {
-            Intent datactivity = new Intent(ScanningActivity.this, PersonalDataActivity.class);
+        IntentResult scanResults = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResults != null){
 
-            startActivity(datactivity);
+            // TODO Send scanned content to server.
 
-        } else if (id == R.id.nav_recommend) {
-            Intent recommendactivity = new Intent(ScanningActivity.this, RecommendationActivity.class);
+            String scanContent = scanResults.getContents();
+            String scanFormat = scanResults.getFormatName();
 
-            startActivity(recommendactivity);
+            format.setText("FORMAT: " + scanFormat);
+            content.setText("CONTENT: " + scanContent);
 
-
-        } else if (id == R.id.nav_rating) {
-            Intent ratingactivity = new Intent(ScanningActivity.this, RatingActivity.class);
-
-            startActivity(ratingactivity);
-
-        } else if (id == R.id.nav_history) {
-            Intent historyactivity = new Intent(ScanningActivity.this, HistoryActivity.class);
-
-            startActivity(historyactivity);
-
+        } else{
+            Toast toast = Toast.makeText(getApplicationContext(), "Scan Cancelled", Toast.LENGTH_SHORT);
+            toast.show();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
