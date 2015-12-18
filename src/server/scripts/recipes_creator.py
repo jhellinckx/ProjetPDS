316aebb1a8149cbc8c23d2331a3ca19129b4e5ee
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2015-12-18 22:49:54 +0100

    fixed bug

--- src/server/scripts/recipes_creator.py
+++ src/server/scripts/recipes_creator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import mysql.connector
 
-cnx = mysql.connector.connect(user='jhellinckx', database='db_appli',password="Immersion33")
+cnx = mysql.connector.connect(user='jhellinckx', database='db_appli',password="...")
 cursor = cnx.cursor()
 
 query = ("SELECT id_food, url, code, product_name, image_url, energy_100g FROM Food")
