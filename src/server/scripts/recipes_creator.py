2f4aea7c167d380d5d211ceb78731dec0fe18398
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2015-12-18 22:48:59 +0100

    basic sql python connector

---
+++ src/server/scripts/recipes_creator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import mysql.connector
+
+cnx = mysql.connector.connect(user='jhellinckx', database='db_appli',password="Immersion33")
+cursor = cnx.cursor()
+
+query = ("SELECT id_food, url, code, product_name, image_url, energy_100g FROM Food")
+
+cursor.execute(query)
+for (id_food, url, code, product_name, image_url, energy_100g) in cursor:
+	print product_name
+cursor.close()
+cnx.close()
