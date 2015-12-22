9c2890219cc514d50517e404c814361ccd89fa46
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2015-12-22 18:25:19 +0100

    [add] operations into building recipe db

--- src/server/scripts/recipes_creator.py
+++ src/server/scripts/recipes_creator.py
@@ -2,6 +2,7 @@
 import sys
 import requests
 import ast
+from bs4 import BeautifulSoup
 
 
 try:
@@ -39,52 +40,54 @@ def build_http_request(api_url, **kwargs):
 		params = params[:-1] # Get rid of last '&'
 	return api_url+params
 
-def ingredients_from_recipepuppy():
-	all_ingredients = []
+def recipes_from_recipepuppy():
+	all_recipes = []
 	for i in range (1, 101):
 		response = requests.get(build_http_request(api, minRates=250,p=i))
 		responseDict = ast.literal_eval(response.content)
 		recipes = responseDict["results"]
 		for recipe in recipes :
-			recipe_ingredients = recipe["ingredients"].split(", ")
-			for ingredient in recipe_ingredients:
-				if ingredient not in all_ingredients:
-					all_ingredients.append(ingredient)
-	f = open("ingredients.txt","w+")
-	f.write(str(all_ingredients))
+			all_recipes.append(recipe)
+	f = open("recipes.txt","w+")
+	f.write(str(all_recipes))
 
-(username, password) = db_params()
+def parse_recipe_info():
+	f = open("allrecipes.txt","r")
+	recipes = ast.literal_eval(f.read())
+	for recipe in recipes :
+		url = recipe["href"].replace("\\","")
+		print url 
 
-cnx = mysql.connector.connect(user=username, database=db_name,password=password)
-cursor = cnx.cursor()
 
-f = open("ingredients.txt","r") 
-to_file = open("results.txt","w+")
-all_ingredients = f.read()[1:-2].split(", ")
-todo = len(all_ingredients)
-inexisting = []
-for ingredient in all_ingredients:
-	ingredient = ingredient.strip("'")
-	ingredient = ingredient.replace(" ","-")
-	query = ("SELECT id_food, generic_name FROM Food WHERE generic_name LIKE '%"+ingredient+"%'")
-	cursor.execute(query)
-	i=0
-	categories = []
-	for (id_food, generic_name) in cursor:
-		if(i==0):
-			to_file.write("INGREDIENT NAME = "+ingredient+"\n")
-		if(generic_name not in categories):
-			to_file.write(generic_name.encode("utf8")+"\n")
-			categories.append(generic_name)
-		i+=1
-	if(i>0):
-		to_file.write("*"*50+"\n")
-	else:
-		inexisting.append(ingredient)
 
-	todo-=1
-	print(str(todo)+" ingredient(s) left")
-to_file.write("INEXISTING INGREDIENTS : "+str(inexisting))
+def found_to_file():
+	(username, password) = db_params()
+	cnx = mysql.connector.connect(user=username, database=db_name,password=password)
+	cursor = cnx.cursor()
+	f = open("ingredients.txt","r") 
+	to_file = open("results.txt","w+")
+	all_ingredients = f.read()[1:-2].split(", ")
+	todo = len(all_ingredients)
+	inexisting = []
+	for ingredient in all_ingredients:
+		ingredient = ingredient.strip("'")
+		ingredientCap = ingredient[0].capitalize() + ingredient[1:]
+		query = ("SELECT id_food, categories, product_name FROM Food WHERE categories LIKE '%"+ingredient+"%' OR categories LIKE '%"+ingredientCap+"%'")
+		cursor.execute(query)
+		i=0
+		for (id_food, categories, product_name) in cursor:
+			if(i==0):
+				to_file.write("INGREDIENT NAME = "+ingredient+"\n")
+				to_file.write(categories.encode('utf-8')+"\n")
+			i+=1
+		if(i>0):
+			to_file.write("*"*50+"\n")
+		else:
+			inexisting.append(ingredient)
 
-cursor.close()
-cnx.close()
+		todo-=1
+		print(str(todo)+" ingredient(s) left")
+	to_file.write("INEXISTING INGREDIENTS : "+str(inexisting))
+
+	cursor.close()
+	cnx.close()
