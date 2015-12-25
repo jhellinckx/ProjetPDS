b7d3b4a7c48dcb8c266261ddf2711d24d25cee9a
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2015-12-25 16:18:08 +0100

    [enh] parsing nutriments and ratings from allrecipes now takes previous parsed infomration into account

--- src/server/scripts/recipes_creator.py
+++ src/server/scripts/recipes_creator.py
@@ -51,14 +51,82 @@ def recipes_from_recipepuppy():
 	f = open("recipes.txt","w+")
 	f.write(str(all_recipes))
 
-def parse_recipe_info():
-	f = open("allrecipes.txt","r")
+def parse_recipe_info(url):
+	res = requests.get(url)
+	parsed_res = BeautifulSoup(res.content)
+	ratingInfos = 	{
+					"rating": 		None,
+					"n_ratings": 	None
+					}	
+	parsed_rating = BeautifulSoup(str(parsed_res.body.find("span",attrs={"class":"aggregate-rating","itemprop":"aggregateRating"})))
+	if parsed_rating != None :
+		meta_tag = parsed_rating.find("meta")
+		if meta_tag != None :
+			ratingInfos["rating"] = meta_tag.attrs["content"]
+		span_tag = parsed_rating.find("span", attrs={"class":"review-count"})
+		if span_tag != None:
+			ratingInfos["n_ratings"] = str(span_tag.text)
+
+
+	nutrInfos = {
+				"calories": 			None, 
+				"fatContent":			None, 
+				"carbohydrateContent":	None,
+				"proteinContent":		None,
+				"cholesterolContent":	None
+				}
+
+	parsed_nutr = BeautifulSoup(str(parsed_res.body.find("div",attrs={"class":"recipe-nutrition__form"})))
+	if parsed_nutr != None:
+		for nutr in nutrInfos.keys():
+			nutr_li_tag = parsed_nutr.find("li",attrs={"itemprop":nutr})
+			if nutr_li_tag != None:
+				nutrInfos[nutr] = str(nutr_li_tag.text)
+
+	return dict(ratingInfos, **nutrInfos)
+
+def parse_allrecipes_info(filename="allrecipes.txt"):
+	CYAN = "\033[36m"
+	GREEN = "\033[32m"
+	RED = "\033[31m"
+	RESET = "\033[0m"
+	f = open(filename,"r")
 	recipes = ast.literal_eval(f.read())
-	for recipe in recipes :
-		url = recipe["href"].replace("\\","")
-		print url 
 
+	# Try to get the already done URLs
+	done_recipes = {}
+	try:
+		doneFile = open(filename.split(".")[0]+"_with_infos"+".txt","r")
+		doneRaw = doneFile.read()
+		for i in range(len(doneRaw)-1, -1, -1): # In case the last writing did not complete the write operation of a recipe
+			if doneRaw[i] == '}':
+				doneRaw = doneRaw[:i+1] + "]"
+				break
+		done_recipes_list = ast.literal_eval(doneRaw)
+		for recipe in done_recipes_list:
+			done_recipes[recipe["href"].replace("\\","")] = recipe
+	except:
+		print RED + "Could not find previous file. Starting fresh..." + RESET
+	finally:
+		doneFile.close()
 
+	toFile = open(filename.split(".")[0]+"_with_infos"+".txt","w+")
+	toFile.write("[")
+	for i in range(len(recipes)) :
+		url = recipes[i]["href"].replace("\\","")
+		if url in done_recipes:
+			recipes[i] = done_recipes[url]
+			print CYAN + str(i+1) + "/" + str(len(recipes)) + RESET + " " + url + " -> " + GREEN + "DONE" + RESET
+		else:
+			infos = parse_recipe_info(url)
+			print CYAN + str(i+1) + "/" + str(len(recipes)) + RESET + " " + url + " -> " + str(infos)
+			recipes[i] = dict(recipes[i], **infos) # Concatenate the dictionaries
+		toFile.write(str(recipes[i]))
+		if(i < len(recipes)-1):
+			toFile.write(",")
+	toFile.write("]")
+	toFile.close()
+	f.close()
 
 def found_to_file():
 	(username, password) = db_params()
