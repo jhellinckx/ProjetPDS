ee56735cd559f5754108e8825022c3bcab8137e4
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2015-12-25 20:10:24 +0100

    [add] allrecipes recipes  inserted in Recipes db table

--- src/server/database/db_recipes.py
+++ src/server/database/db_recipes.py
@@ -11,8 +11,8 @@ except: # no mysql.connector
 	print "Error : need MySQL python connector (provided by Oracle at https://dev.mysql.com/downloads/connector/python/)"
 	sys.exit()
 
-BLUE = "\033[34m"
-CYAN = "\033[36m"
+BLUE = "\033[33m"
+CYAN = "\033[35m"
 GREEN = "\033[32m"
 RED = "\033[31m"
 RESET = "\033[0m"
@@ -100,8 +100,8 @@ def add_recipes_in_db():
 	recipes_table_command = (
     	"CREATE TABLE `Recipes` ("
     	"  `id` int(11) NOT NULL AUTO_INCREMENT,"
-    	"  `rating` decimal(3,2) NOT NULL,"
-    	"  `n_ratings` int(11) NOT NULL,"
+    	"  `rating` varchar(16) NOT NULL,"
+    	"  `n_ratings` varchar(16) NOT NULL,"
     	"  `calories` varchar(16) NOT NULL,"
     	"  `fatContent` varchar(16) NOT NULL,"
     	"  `carbsContent` varchar(16) NOT NULL,"
@@ -109,9 +109,18 @@ def add_recipes_in_db():
     	"  `proteinContent` varchar(16) NOT NULL,"
     	"  `url` varchar(255) NOT NULL,"
     	"  `thumbnail` varchar(255) NOT NULL,"
-    	"  `title` varchar(100) NOT NULL,"
+    	"  `title` varchar(255) NOT NULL,"
     	"  PRIMARY KEY (`id`)"
     	") ENGINE=InnoDB")
+
+	recipes_insert_command = (
+		"INSERT INTO Recipes "
+		"(rating, n_ratings, calories, fatContent,"
+		" carbsContent, cholesterolContent, proteinContent,"
+		" url, thumbnail, title)"
+		"VALUES (%(rating)s, %(n_ratings)s, %(calories)s, %(fatContent)s,"
+		" %(carbohydrateContent)s, %(cholesterolContent)s, %(proteinContent)s,"
+		" %(href)s, %(thumbnail)s, %(title)s)")
 
 	recipes_ingredients_lists_table_command = (	# Many-to-many recipes <-> ingredients
     	"CREATE TABLE `RecipesIngredientsLists` ("
@@ -149,10 +158,25 @@ def add_recipes_in_db():
 	except mysql.connector.Error as err:
 		sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
 
+	# Insert Recipes into table
+	recipes_file = open(recipes_filename, "r")
+	recipes_list = ast.literal_eval(recipes_file.read())
+	recipes_file.close()
+	for recipe in recipes_list:
+		ingredients_list = recipe.pop("ingredients", None) # Remove ingredients key to use it in RecipesIngredientsLists insert
+		sys.stdout.write("Inserting " + CYAN + recipe["title"] + RESET + " into " + BLUE + "Recipes" + RESET + "... ")
+		try:
+			cursor.execute(recipes_insert_command, recipe)
+			sys.stdout.write(GREEN + "OK" + RESET + "\n")
+		except mysql.connector.Error as err:
+			sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
+
+
 	cnx.commit()
 	cursor.close()
 	cnx.close()
 
 drop_added_tables_in_db()
+add_ingredients_in_db()
 add_recipes_in_db()
 
