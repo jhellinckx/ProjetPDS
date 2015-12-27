26a09edb64e1527de8450d53ed3b3de187ad8731
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2015-12-27 22:44:26 +0100

    [add] selector for input box and button

--- src/android client/app/src/main/java/com/pds/app/caloriecounter/NotifiableActivity.java
+++ src/android client/app/src/main/java/com/pds/app/caloriecounter/NotifiableActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
