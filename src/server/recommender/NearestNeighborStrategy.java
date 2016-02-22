52e4b49e7ba096fee3bce83f0057f25300d5607a
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-02-22 19:10:39 +0100

    [add] Base CB strategy logic

--- src/server/recommender/NearestNeighborStrategy.java
+++ src/server/recommender/NearestNeighborStrategy.java
@@ -1,33 +1,54 @@
 package recommender;
 
-import java.util.ArrayList;
+import java.util.*;
 import items.Food;
+import dao.CategoryRatingDAOImpl;
 
 public class NearestNeighborStrategy extends ContentBasedStrategy {
 	private ArrayList<Food> _foodToFilter;
 	private User _user;
-	private int _recommandations;
+	private int _recommendations;
+	private CategoryRatingDAOImpl _daoCategoryRating;
+
+	NearestNeighborStrategy(CategoryRatingDAOImpl daoCategoryRating){
+		_daoCategoryRating = daoCategoryRating;
+	}
 
 	@Override
 	public void updateData(ArrayList<Food> toFilter, User user, int recoms){  
 		_foodToFilter = toFilter;
 		_user = user;
-		_recommandations = recoms;
+		_recommendations = recoms;
 	}
 
-
-	/* Called if no filtering is required. In that case, the system has to explore
-	the database to find relevant food and mark them */
 	@Override
 	public ArrayList<Food> recommend(){
-
-		return new ArrayList<Food>();
-		
+		Map<Food, Float> ratingPredictions = new HashMap<>();
+		for(Food food : _foodToFilter){
+			ratingPredictions.put(food, prediction(food));
+		}
+		Map<Food, Float> sortedRatingPredictions = sortByValue(ratingPredictions);
+		List<Food> recommendations = new ArrayList<Food>(sortedRatingPredictions.keySet())
+		return recommendations.subList(0, _recommendations);
 	}
 
-	/*  */
-	public ArrayList<Food> recommend(ArrayList<Food> toFilter){
+	private Float prediction(Food food){
 
+	}
+
+	public <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+		List<Map.Entry<K, V>> entryList = new LinkedList<>(map.entrySet());
+		Collections.sort(entryList, new Comparator<Map.Entry<K, V>>(){
+        	@Override
+        	public int compare(Map.Entry<K, V> first, Map.Entry<K, V> second){
+            	return (first.getValue()).compareTo(second.getValue());
+        	}
+    	});
+		Map<K, V> sortedMap = new LinkedHashMap<>();
+		for (Map.Entry<K, V> entry : entryList){
+        	sortedMap.put(entry.getKey(), entry.getValue());
+        }
+		return sortedMap;
 	}
 
 }
