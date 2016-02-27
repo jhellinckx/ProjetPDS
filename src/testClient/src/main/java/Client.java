5b3ee982b3033a786ca6b246c469683844e54eee
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-02-27 02:13:50 +0100

    [add] testClient should work with gradle and intelliJ. Make commands in src for now

---
+++ src/testClient/src/main/java/Client.java
@@ -0,0 +1,7 @@
+import org.calorycounter.shared.Constants;
+
+public class Client{
+    public static final void main(String[] args){
+        System.out.println("Hello World !" +Constants.network.FOOD_URL);
+    }
+}
