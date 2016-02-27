f6129d2217c58ba049b700c145afca62e1705a75
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-02-27 20:23:43 +0100

    [add] prices parsed

--- src/server/scripts/colruyt_getter.py
+++ src/server/scripts/colruyt_getter.py
@@ -5,6 +5,10 @@ import sys
 import requests
 import ast
 import re
+import codecs
+import base64
+
+from time import sleep
 
 from bs4 import BeautifulSoup
 
@@ -45,9 +49,10 @@ class Article:
 def all_items_from_branch(branch_index):
 	# Always get cookies
 	cookies = requests.get(cookie_setup_url).cookies
-	structured_response = BeautifulSoup(requests.get(search_by_branch_url + str(branch_index), cookies=cookies).content, "lxml")
+	raw_response = requests.get(search_by_branch_url + str(branch_index), cookies=cookies)
+	structured_response = BeautifulSoup(raw_response.content, "lxml")
 	articles = []
-	
+
 	# All attributes
 	image_url = None; price_unit = None; price_kg = None; details_url = None; categories=[];
 
@@ -60,13 +65,30 @@ def all_items_from_branch(branch_index):
 		for elem in script_dict.split(","):
 			if "page_cat" in elem:
 				categories.append(elem.split(":")[1].strip(" ").strip("'"))
-
 	# Find rest
 	for article in structured_response.find("div",attrs={"id":"articles"}).children:
-		article_image_wrap = article.find("div",attr={"class":"imageWrap"})
+		# Image url
+		article_image_wrap = article.find("div",attrs={"class":"imageWrap"})
 		if(article_image_wrap != None):
-			image_url = domain + str(article_image_wrap.find("img").attrs["src"])  
+			image_url = domain + str(article_image_wrap.find("img").attrs["src"]) 
 
+		# Price 
+		article_price_div = article.find("div", attrs={"class":"price"})
+		if article_price_div != None:
+			article_price_unit = article_price_div.find("p", attrs={"class":"piece"})
+			if article_price_unit != None:
+				script_tag = article_price_unit.find("script")
+				if script_tag != None:
+					print base64.b64decode(script_tag.string.split(",")[0].split("(")[1].strip("'"))
+				
+
+			article_price_kg = article_price_div.find("p", attrs={"class":"unit"})
+			if article_price_kg != None:
+				script_tag = article_price_kg.find("script")
+				if script_tag != None:
+					print base64.b64decode(script_tag.string.split(",")[0].split("(")[1].strip("'"))
+				
+		print("-"*5)
 	with open(last_cookies_filename,"w+") as f:
 		f.write(str(cookies))
 
