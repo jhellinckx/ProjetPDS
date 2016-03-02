fdcffbed671a9f6368b48269de1a723cc521b405
Aurelien van Delft <avdelft@ulb.ac.be>
2016-03-02 16:24:16 +0100

    sticker background

--- src/android client/app/src/main/java/com/pds/app/caloriecounter/ItemSticker.java
+++ src/android client/app/src/main/java/com/pds/app/caloriecounter/ItemSticker.java
@@ -1,12 +1,20 @@
 package com.pds.app.caloriecounter;
 
 import android.content.Context;
-import android.view.Gravity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 public class ItemSticker extends LinearLayout {
 
@@ -18,6 +26,7 @@ public class ItemSticker extends LinearLayout {
         super(cont);
         context = cont;
         this.setOrientation(LinearLayout.HORIZONTAL);
+        this.setBackground(context.getResources().getDrawable(R.drawable.background_sticker));
         initThumbnail(url);
         addDescribingText(txt);
     }
@@ -27,13 +36,16 @@ public class ItemSticker extends LinearLayout {
         Picasso.with(context)
                 .load(url)
                 .resize(200,200)
+                .transform(new RoundedTransformation(5,0))
                 .into(thumbnail);
+        thumbnail.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
         this.addView(thumbnail, 0);
     }
 
-    private void addDescribingText(String txt){
+    private void addDescribingText(String txt) {
         describing_text = new TextView(context);
         describing_text.setText(txt);
-        this.addView(describing_text,1);
+        this.addView(describing_text, 1);
     }
 }
