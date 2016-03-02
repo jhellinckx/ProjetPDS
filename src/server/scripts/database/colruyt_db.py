8b4e3fdeacae282643c01e559d684582ad46b396
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-03-02 15:26:45 +0100

    [add] Colruyt database creation should be functional

--- src/server/scripts/database/colruyt_db.py
+++ src/server/scripts/database/colruyt_db.py
@@ -18,25 +18,7 @@ db_name = "db_colruyt"
 db_encoding = "utf8"
 db_properties_filename = "../../src/main/resources/dao.properties"
 
-def db_params():
-	username = None
-	password = None
-	f = open(db_properties_filename,"r")
-	for line in f:
-		line = line.strip().split(" ")
-		if line[0] == "username":
-			username = line[-1]
-		elif line[0] == "password":
-			password = line[-1]
-	f.close()
-	return (username, password)
-
-def create_food_table():
-	(username, password) = db_params()
-	cnx = mysql.connector.connect(user=username, password=password, database=db_name)
-	cursor = cnx.cursor()
-
-	articles_names_correction = \
+articles_names_correction = \
 			{
 				BaseArticle.BRANCH_INDEX_KEY 						: BaseArticle.BRANCH_INDEX_KEY,\
 				BaseArticle.IMAGE_KEY 								: "image_url",\
@@ -74,12 +56,12 @@ def create_food_table():
 				DetailedArticle.ALLERGENS_TRACE_OF_KEY 				: "traces",\
 			}
 
-	articles_details_sql_types = \
+articles_details_sql_types = \
 			{
 				BaseArticle.BRANCH_INDEX_KEY 						: "INT UNSIGNED",\
 				BaseArticle.IMAGE_KEY 								: "VARCHAR(255)",\
-				BaseArticle.PRICE_UNIT_KEY 							: "FLOAT",\
-				BaseArticle.PRICE_KG_KEY 							: "FLOAT",\
+				BaseArticle.PRICE_UNIT_KEY 							: "VARCHAR(10)",\
+				BaseArticle.PRICE_KG_KEY 							: "VARCHAR(10)",\
 				BaseArticle.DETAILS_URL_KEY 						: "VARCHAR(255)",\
 				BaseArticle.CATEGORIES_KEY 							: "TEXT",\
 				BaseArticle.WEIRD_NAME_KEY 							: "VARCHAR(255)",\
@@ -95,7 +77,7 @@ def create_food_table():
 				DetailedArticle.PORTION_TOTAL_PROTEINS_KEY 			: "VARCHAR(16)",\
 				DetailedArticle.PORTION_FIBERS_KEY 					: "VARCHAR(16)",\
 				DetailedArticle.PORTION_SALT_KEY 					: "VARCHAR(16)",\
-				DetailedArticle.PORTION_INFO_KEY 					: "VARCHAR(16)",\
+				DetailedArticle.PORTION_INFO_KEY 					: "VARCHAR(255)",\
 				DetailedArticle.PER_100G_ENERGY_KJ_KEY 				: "VARCHAR(16)",\
 				DetailedArticle.PER_100G_ENERGY_KCAL_KEY 			: "VARCHAR(16)",\
 				DetailedArticle.PER_100G_TOTAL_FAT_KEY 				: "VARCHAR(16)",\
@@ -112,59 +94,117 @@ def create_food_table():
 				DetailedArticle.ALLERGENS_TRACE_OF_KEY 				: "TEXT",\
 			}
 
-	food_table_command = "CREATE TABLE `Food` ("
-	food_table_command += "`id_food` INT UNSIGNED NOT NULL AUTO_INCREMENT, "
-	for db_entry in articles_details_sql_types : 
-		food_table_command +=  "`" + articles_names_correction[db_entry] + "`" + " " + articles_details_sql_types[db_entry] + ", " 
-	food_table_command += "PRIMARY KEY (`id_food`)) ENGINE=InnoDB"
-	sys.stdout.write("Creating " + MAGENTA + "Food" + RESET + " table... ")
-	try:
-		cursor.execute(food_table_command)
-		sys.stdout.write(GREEN + "OK" + RESET + "\n")
-	except mysql.connector.Error as err:
-		sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
+def db_params():
+	username = None
+	password = None
+	f = open(db_properties_filename,"r")
+	for line in f:
+		line = line.strip().split(" ")
+		if line[0] == "username":
+			username = line[-1]
+		elif line[0] == "password":
+			password = line[-1]
+	f.close()
+	return (username, password)
 
-	cnx.commit()
-	cursor.close()
-	cnx.close()
-
-def add_items():
+def drop_db():
 	(username, password) = db_params()
+	drop_command = "DROP DATABASE " + db_name + ";"
 	cnx = mysql.connector.connect(user=username, password=password)
 	cursor = cnx.cursor()
-
-
-	articles = []
-	with open(details_results_filename, "r") as f:
-		for line in details_results_filename:
-			articles.append(ast.literal_eval(line.rstrip("\n")))
-	for article in articles:
-		pass
-
+	cursor.execute(drop_command)
 	cnx.commit()
 	cursor.close()
 	cnx.close()
 
 def create_db():
 	(username, password) = db_params()
-	drop_command = "DROP DATABASE " + db_name + ";"
 	create_command = "CREATE DATABASE " + db_name + " CHARACTER SET " + db_encoding + ";"
 	cnx = mysql.connector.connect(user=username, password=password)
 	cursor = cnx.cursor()
-	try:
-		sys.stdout.write("Dropping database " + MAGENTA + db_name + RESET + "... ")
-		cursor.execute(drop_command)
-		sys.stdout.write(GREEN + "OK " + RESET + "\n")
-	except mysql.connector.Error as err:
-		sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
-	finally:
-		sys.stdout.write("Creating database " + MAGENTA + db_name + RESET + "... ")
-		cursor.execute(create_command)
-		sys.stdout.write(GREEN + "OK " + RESET + "\n")
+	cursor.execute(create_command)
 	cnx.commit()
 	cursor.close()
 	cnx.close()
 
+def create_food_table():
+	food_table_command = "CREATE TABLE `Food` ("
+	food_table_command += "`id_food` INT UNSIGNED NOT NULL AUTO_INCREMENT, "
+	for db_entry in articles_details_sql_types : 
+		food_table_command +=  "`" + articles_names_correction[db_entry] + "`" + " " + articles_details_sql_types[db_entry] + ", " 
+	food_table_command += "PRIMARY KEY (`id_food`)) ENGINE=InnoDB"
+
+	(username, password) = db_params()
+	cnx = mysql.connector.connect(user=username, password=password, database=db_name)
+	cursor = cnx.cursor()
+	cursor.execute(food_table_command)
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
+def add_items_in_food():
+	article_insert_command = "INSERT INTO Food "
+	db_names = "("
+	values = "VALUES ("
+	for key in articles_names_correction :
+		db_names += articles_names_correction[key] + ", "
+		values += "%(" + key + ")s, "
+	db_names = db_names[:-2] + ") "
+	values = values[:-2] +")"
+	article_insert_command += db_names + values
+
+	(username, password) = db_params()
+	cnx = mysql.connector.connect(user=username, password=password,database=db_name)
+	cursor = cnx.cursor()
+	with open(details_results_filename, "r") as f:
+		for line in f:
+			article = ast.literal_eval(line.rstrip("\n"))
+			for key in article:
+				if isinstance(article[key], list):
+					article[key] = str(article[key])[1:-1] # Get rid of [ and ] of list string
+					if len(article[key]) == 0:
+						article[key] = None
+			cursor.execute(article_insert_command, article)
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
+
+
 if __name__ == "__main__" :
-	create_db()
-	create_food_table()
+	try:
+		try:
+			sys.stdout.write("Dropping database " + MAGENTA + db_name + RESET + "... ")
+			sys.stdout.flush()
+			drop_db()
+			sys.stdout.write(GREEN + "OK " + RESET + "\n")
+			sys.stdout.flush()
+		except mysql.connector.Error as err:
+			sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
+			sys.stdout.flush()
+		finally:
+			sys.stdout.write("Creating database " + MAGENTA + db_name + RESET + "... ")
+			sys.stdout.flush()
+			create_db()
+			sys.stdout.write(GREEN + "OK " + RESET + "\n")
+			sys.stdout.flush()
+			sys.stdout.write("Creating " + MAGENTA + "Food" + RESET + " table... ")
+			sys.stdout.flush()
+			create_food_table()
+			sys.stdout.write(GREEN + "OK" + RESET + "\n")
+			sys.stdout.flush()
+			sys.stdout.write("Inserting items in " + MAGENTA + "Food" + RESET + "... ")
+			sys.stdout.flush()
+			add_items_in_food()
+			sys.stdout.write(GREEN + "OK" + RESET + "\n")
+			sys.stdout.flush()
+	except mysql.connector.Error as err:
+		sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
+		sys.stdout.flush()
+
+
+
+
+
+
+
