18151ab88e39c5803581d3fd438954fe39e3af0e
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-03-02 02:33:32 +0100

    [add] py dict to easily generate sql commands

--- src/server/scripts/database/colruyt_db.py
+++ src/server/scripts/database/colruyt_db.py
@@ -31,6 +31,108 @@ def db_params():
 	f.close()
 	return (username, password)
 
+def create_food_table():
+	(username, password) = db_params()
+	cnx = mysql.connector.connect(user=username, password=password)
+	cursor = cnx.cursor()
+
+	names_correction = 
+			{
+				BaseArticle.BRANCH_INDEX_KEY 						: BaseArticle.BRANCH_INDEX_KEY,\
+				BaseArticle.IMAGE_KEY 								: "image_url",\
+				BaseArticle.PRICE_UNIT_KEY 							: BaseArticle.PRICE_UNIT_KEY,\
+				BaseArticle.PRICE_KG_KEY 							: BaseArticle.PRICE_KG_KEY,\
+				BaseArticle.DETAILS_URL_KEY 						: "url",\
+				BaseArticle.CATEGORIES_KEY 							: "categories",\
+				BaseArticle.WEIRD_NAME_KEY 							: "brand",\
+				BaseArticle.SHORT_DESCRIPTION_KEY					: "product_name",\
+				BaseArticle.FULL_DESCRIPTION_KEY 					: BaseArticle.FULL_DESCRIPTION_KEY,\
+				DetailedArticle.REAL_DETAILS_URL_KEY 				: DetailedArticle.REAL_DETAILS_URL_KEY,\
+				DetailedArticle.PORTION_ENERGY_KJ_KEY 				: DetailedArticle.PORTION_ENERGY_KJ_KEY,\
+				DetailedArticle.PORTION_ENERGY_KCAL_KEY 			: DetailedArticle.PORTION_ENERGY_KCAL_KEY,\
+				DetailedArticle.PORTION_TOTAL_FAT_KEY 				: DetailedArticle.PORTION_TOTAL_FAT_KEY,\
+				DetailedArticle.PORTION_SATURATED_FAT_KEY 			: DetailedArticle.PORTION_SATURATED_FAT_KEY,\
+				DetailedArticle.PORTION_TOTAL_CARBOHYDRATES_KEY 	: DetailedArticle.PORTION_TOTAL_CARBOHYDRATES_KEY,\
+				DetailedArticle.PORTION_SUGARS_KEY 					: DetailedArticle.PORTION_SUGARS_KEY,\
+				DetailedArticle.PORTION_TOTAL_PROTEINS_KEY 			: DetailedArticle.PORTION_TOTAL_PROTEINS_KEY,\
+				DetailedArticle.PORTION_FIBERS_KEY 					: DetailedArticle.PORTION_FIBERS_KEY,\
+				DetailedArticle.PORTION_SALT_KEY 					: DetailedArticle.PORTION_SALT_KEY,\
+				DetailedArticle.PORTION_INFO_KEY 					: DetailedArticle.PORTION_INFO_KEY,\
+				DetailedArticle.PER_100G_ENERGY_KJ_KEY 				: "energy_100g",\
+				DetailedArticle.PER_100G_ENERGY_KCAL_KEY 			: DetailedArticle.PER_100G_ENERGY_KCAL_KEY,\
+				DetailedArticle.PER_100G_TOTAL_FAT_KEY 				: "fat_100g",\
+				DetailedArticle.PER_100G_SATURATED_FAT_KEY 			: "saturated_fat_100g",\
+				DetailedArticle.PER_100G_TOTAL_CARBOHYDRATES_KEY 	: "carbohydrates_100g",\
+				DetailedArticle.PER_100G_SUGARS_KEY 				: "sugars_100g",\
+				DetailedArticle.PER_100G_TOTAL_PROTEINS_KEY 		: "proteins_100g",\
+				DetailedArticle.PER_100G_FIBERS_KEY 				: "fiber_100g",\
+				DetailedArticle.PER_100G_SALT_KEY					: "salt_100g",\
+				DetailedArticle.TOTAL_QUANTITY_KEY 					: "quantity",\
+				DetailedArticle.INGREDIENTS_TEXT_KEY 				: "ingredients_text",\
+				DetailedArticle.BAR_CODE_KEY 						: "code",\
+				DetailedArticle.ALLERGENS_CONTAINS_KEY 				: "allergens",\
+				DetailedArticle.ALLERGENS_TRACE_OF_KEY 				: "traces",\
+			}
+
+	sql_types = 
+			{
+				BaseArticle.BRANCH_INDEX_KEY 						: "UNSIGNED",\
+				BaseArticle.IMAGE_KEY 								: "VARCHAR(255)",\
+				BaseArticle.PRICE_UNIT_KEY 							: "FLOAT",\
+				BaseArticle.PRICE_KG_KEY 							: "FLOAT",\
+				BaseArticle.DETAILS_URL_KEY 						: "VARCHAR(255)",\
+				BaseArticle.CATEGORIES_KEY 							: "TEXT",\
+				BaseArticle.WEIRD_NAME_KEY 							: "VARCHAR(255)",\
+				BaseArticle.SHORT_DESCRIPTION_KEY					: "VARCHAR(255)",\
+				BaseArticle.FULL_DESCRIPTION_KEY 					: "TEXT",\
+				DetailedArticle.REAL_DETAILS_URL_KEY 				: "VARCHAR(255)",\
+				DetailedArticle.PORTION_ENERGY_KJ_KEY 				: "VARCHAR(10)",\
+				DetailedArticle.PORTION_ENERGY_KCAL_KEY 			: "VARCHAR(10)",\
+				DetailedArticle.PORTION_TOTAL_FAT_KEY 				: "VARCHAR(10)",\
+				DetailedArticle.PORTION_SATURATED_FAT_KEY 			: "VARCHAR(10)",\
+				DetailedArticle.PORTION_TOTAL_CARBOHYDRATES_KEY 	: "VARCHAR(10)",\
+				DetailedArticle.PORTION_SUGARS_KEY 					: "VARCHAR(10)",\
+				DetailedArticle.PORTION_TOTAL_PROTEINS_KEY 			: "VARCHAR(10)",\
+				DetailedArticle.PORTION_FIBERS_KEY 					: "VARCHAR(10)",\
+				DetailedArticle.PORTION_SALT_KEY 					: "VARCHAR(10)",\
+				DetailedArticle.PORTION_INFO_KEY 					: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_ENERGY_KJ_KEY 				: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_ENERGY_KCAL_KEY 			: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_TOTAL_FAT_KEY 				: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_SATURATED_FAT_KEY 			: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_TOTAL_CARBOHYDRATES_KEY 	: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_SUGARS_KEY 				: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_TOTAL_PROTEINS_KEY 		: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_FIBERS_KEY 				: "VARCHAR(10)",\
+				DetailedArticle.PER_100G_SALT_KEY					: "VARCHAR(10)",\
+				DetailedArticle.TOTAL_QUANTITY_KEY 					: "VARCHAR(255)",\
+				DetailedArticle.INGREDIENTS_TEXT_KEY 				: "TEXT",\
+				DetailedArticle.BAR_CODE_KEY 						: "VARCHAR(255)",\
+				DetailedArticle.ALLERGENS_CONTAINS_KEY 				: "TEXT",\
+				DetailedArticle.ALLERGENS_TRACE_OF_KEY 				: "TEXT",\
+			}
+
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
+def add_items():
+	(username, password) = db_params()
+	cnx = mysql.connector.connect(user=username, password=password)
+	cursor = cnx.cursor()
+
+
+	articles = []
+	with open(details_results_filename, "r") as f:
+		for line in details_results_filename:
+			articles.append(ast.literal_eval(line.rstrip("\n")))
+	for article in articles:
+		pass
+
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
 def create_db():
 	(username, password) = db_params()
 	drop_command = "DROP DATABASE " + db_name + ";"
