b3dbea6a66262d4e695bf658acf1b12db8ea3ed9
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-03-02 03:08:57 +0100

    [add] table creation after fucking gjlrgnreglelngm

--- src/server/scripts/database/colruyt_db.py
+++ src/server/scripts/database/colruyt_db.py
@@ -33,10 +33,10 @@ def db_params():
 
 def create_food_table():
 	(username, password) = db_params()
-	cnx = mysql.connector.connect(user=username, password=password)
+	cnx = mysql.connector.connect(user=username, password=password, database=db_name)
 	cursor = cnx.cursor()
 
-	names_correction = 
+	articles_names_correction = \
 			{
 				BaseArticle.BRANCH_INDEX_KEY 						: BaseArticle.BRANCH_INDEX_KEY,\
 				BaseArticle.IMAGE_KEY 								: "image_url",\
@@ -74,9 +74,9 @@ def create_food_table():
 				DetailedArticle.ALLERGENS_TRACE_OF_KEY 				: "traces",\
 			}
 
-	sql_types = 
+	articles_details_sql_types = \
 			{
-				BaseArticle.BRANCH_INDEX_KEY 						: "UNSIGNED",\
+				BaseArticle.BRANCH_INDEX_KEY 						: "INT UNSIGNED",\
 				BaseArticle.IMAGE_KEY 								: "VARCHAR(255)",\
 				BaseArticle.PRICE_UNIT_KEY 							: "FLOAT",\
 				BaseArticle.PRICE_KG_KEY 							: "FLOAT",\
@@ -86,31 +86,43 @@ def create_food_table():
 				BaseArticle.SHORT_DESCRIPTION_KEY					: "VARCHAR(255)",\
 				BaseArticle.FULL_DESCRIPTION_KEY 					: "TEXT",\
 				DetailedArticle.REAL_DETAILS_URL_KEY 				: "VARCHAR(255)",\
-				DetailedArticle.PORTION_ENERGY_KJ_KEY 				: "VARCHAR(10)",\
-				DetailedArticle.PORTION_ENERGY_KCAL_KEY 			: "VARCHAR(10)",\
-				DetailedArticle.PORTION_TOTAL_FAT_KEY 				: "VARCHAR(10)",\
-				DetailedArticle.PORTION_SATURATED_FAT_KEY 			: "VARCHAR(10)",\
-				DetailedArticle.PORTION_TOTAL_CARBOHYDRATES_KEY 	: "VARCHAR(10)",\
-				DetailedArticle.PORTION_SUGARS_KEY 					: "VARCHAR(10)",\
-				DetailedArticle.PORTION_TOTAL_PROTEINS_KEY 			: "VARCHAR(10)",\
-				DetailedArticle.PORTION_FIBERS_KEY 					: "VARCHAR(10)",\
-				DetailedArticle.PORTION_SALT_KEY 					: "VARCHAR(10)",\
-				DetailedArticle.PORTION_INFO_KEY 					: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_ENERGY_KJ_KEY 				: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_ENERGY_KCAL_KEY 			: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_TOTAL_FAT_KEY 				: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_SATURATED_FAT_KEY 			: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_TOTAL_CARBOHYDRATES_KEY 	: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_SUGARS_KEY 				: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_TOTAL_PROTEINS_KEY 		: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_FIBERS_KEY 				: "VARCHAR(10)",\
-				DetailedArticle.PER_100G_SALT_KEY					: "VARCHAR(10)",\
+				DetailedArticle.PORTION_ENERGY_KJ_KEY 				: "VARCHAR(16)",\
+				DetailedArticle.PORTION_ENERGY_KCAL_KEY 			: "VARCHAR(16)",\
+				DetailedArticle.PORTION_TOTAL_FAT_KEY 				: "VARCHAR(16)",\
+				DetailedArticle.PORTION_SATURATED_FAT_KEY 			: "VARCHAR(16)",\
+				DetailedArticle.PORTION_TOTAL_CARBOHYDRATES_KEY 	: "VARCHAR(16)",\
+				DetailedArticle.PORTION_SUGARS_KEY 					: "VARCHAR(16)",\
+				DetailedArticle.PORTION_TOTAL_PROTEINS_KEY 			: "VARCHAR(16)",\
+				DetailedArticle.PORTION_FIBERS_KEY 					: "VARCHAR(16)",\
+				DetailedArticle.PORTION_SALT_KEY 					: "VARCHAR(16)",\
+				DetailedArticle.PORTION_INFO_KEY 					: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_ENERGY_KJ_KEY 				: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_ENERGY_KCAL_KEY 			: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_TOTAL_FAT_KEY 				: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_SATURATED_FAT_KEY 			: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_TOTAL_CARBOHYDRATES_KEY 	: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_SUGARS_KEY 				: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_TOTAL_PROTEINS_KEY 		: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_FIBERS_KEY 				: "VARCHAR(16)",\
+				DetailedArticle.PER_100G_SALT_KEY					: "VARCHAR(16)",\
 				DetailedArticle.TOTAL_QUANTITY_KEY 					: "VARCHAR(255)",\
 				DetailedArticle.INGREDIENTS_TEXT_KEY 				: "TEXT",\
 				DetailedArticle.BAR_CODE_KEY 						: "VARCHAR(255)",\
 				DetailedArticle.ALLERGENS_CONTAINS_KEY 				: "TEXT",\
 				DetailedArticle.ALLERGENS_TRACE_OF_KEY 				: "TEXT",\
 			}
+
+	food_table_command = "CREATE TABLE `Food` ("
+	food_table_command += "`id_food` INT UNSIGNED NOT NULL AUTO_INCREMENT, "
+	for db_entry in articles_details_sql_types : 
+		food_table_command +=  "`" + articles_names_correction[db_entry] + "`" + " " + articles_details_sql_types[db_entry] + ", " 
+	food_table_command += "PRIMARY KEY (`id_food`)) ENGINE=InnoDB"
+	sys.stdout.write("Creating " + MAGENTA + "Food" + RESET + " table... ")
+	try:
+		cursor.execute(food_table_command)
+		sys.stdout.write(GREEN + "OK" + RESET + "\n")
+	except mysql.connector.Error as err:
+		sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
 
 	cnx.commit()
 	cursor.close()
@@ -140,20 +152,19 @@ def create_db():
 	cnx = mysql.connector.connect(user=username, password=password)
 	cursor = cnx.cursor()
 	try:
+		sys.stdout.write("Dropping database " + MAGENTA + db_name + RESET + "... ")
 		cursor.execute(drop_command)
+		sys.stdout.write(GREEN + "OK " + RESET + "\n")
 	except mysql.connector.Error as err:
-	    if err.errno == errorcode.ER_BAD_DB_ERROR:
-	        sys.stdout.write("Could not drop database " + MAGENTA + db_name + RESET + "\n")
-	    else:
-	        print(err)
-	        exit(1)
+		sys.stdout.write(RED + "FAILED : %s"%err + RESET + "\n")
 	finally:
 		sys.stdout.write("Creating database " + MAGENTA + db_name + RESET + "... ")
 		cursor.execute(create_command)
-		sys.stdout.write(GREEN + "OK " + RESET)
+		sys.stdout.write(GREEN + "OK " + RESET + "\n")
 	cnx.commit()
 	cursor.close()
 	cnx.close()
 
 if __name__ == "__main__" :
 	create_db()
+	create_food_table()
