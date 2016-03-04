37f7d0c30a23d793273d37a33fc51beaba7f2eb3
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-03-04 22:35:01 +0100

    [enh] container for elem in DayRecording (TODO Refactor this shit)

--- src/android client/app/src/main/java/com/pds/app/caloriecounter/DayRecordingActivity.java
+++ src/android client/app/src/main/java/com/pds/app/caloriecounter/DayRecordingActivity.java
@@ -36,9 +36,9 @@ public class DayRecordingActivity extends MenuNavigableActivity  {
     }
     private class DayRecordingContainer extends LinearLayout {
         private TextView title = null;
-        private LinearLayout content = null;
+        private View content = null;
 
-        public DayRecordingContainer(Context context, String title, LinearLayout content){
+        public DayRecordingContainer(Context context, String title, View content){
             super(context);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
             int marginLeft = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
@@ -47,7 +47,8 @@ public class DayRecordingActivity extends MenuNavigableActivity  {
             int marginBot = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
             params.setMargins(marginLeft, marginTop, marginRight, marginBot);
             this.setLayoutParams(params);
-            this.setBackgroundColor(getResources().getColor(R.color.snowy_mint));
+            this.setOrientation(LinearLayout.VERTICAL);
+            this.setBackgroundColor(getResources().getColor(R.color.white));
             setTitle(title);
             setContent(content);
         }
@@ -62,19 +63,19 @@ public class DayRecordingActivity extends MenuNavigableActivity  {
             this.title.setTextAppearance(getContext(), android.R.style.TextAppearance_DeviceDefault_Small);
             this.title.setTextColor(Color.LTGRAY);
             this.title.setGravity(Gravity.LEFT);
-            int marginLeft = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
-            int marginTop = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
-            int marginRight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
-            int marginBot = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
+            int marginLeft = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
+            int marginTop = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
+            int marginRight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
+            int marginBot = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
             LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
             layoutParams.setMargins(marginLeft, marginTop, marginRight, marginBot);
             this.title.setLayoutParams(layoutParams);
             if(added) this.addView(this.title);
         }
 
-        public void setContent(LinearLayout content){
+        public void setContent(View content){
             this.content = content;
-            this.addView(content);
+            this.addView(this.content);
         }
     }
 
@@ -107,7 +108,13 @@ public class DayRecordingActivity extends MenuNavigableActivity  {
 
         // Add horizontal layout to draw the daily intakes
         intakesLayoutWrapper = new LinearLayout(this);
-        intakesLayoutWrapper.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        int marginLeft = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+        int marginTop = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+        int marginRight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+        int marginBot = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+        params.setMargins(marginLeft,marginTop,marginRight,marginBot);
+        intakesLayoutWrapper.setLayoutParams(params);
         intakesLayoutWrapper.setOrientation(LinearLayout.HORIZONTAL);
 
         dailyIntakes = new LinkedHashMap<>();
@@ -128,9 +135,10 @@ public class DayRecordingActivity extends MenuNavigableActivity  {
         }
         intakesLayoutWrapper.addView(new SpaceView(this));
 
+
+
         DayRecordingContainer intakesContainer = new DayRecordingContainer(this, "APPORTS JOURNALIERS", intakesLayoutWrapper);
         topLayout.addView(intakesContainer);
-
 
     }
 
