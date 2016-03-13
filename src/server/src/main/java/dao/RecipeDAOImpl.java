f58f92f291bcc3d00fabc7ca9194501fe30864f1
mrmmtb <hermanthomas9@outlook.com>
2016-03-13 12:48:30 +0100

    Added getREcipeIdsByCategory for ratingACtivity

--- src/server/src/main/java/dao/RecipeDAOImpl.java
+++ src/server/src/main/java/dao/RecipeDAOImpl.java
@@ -23,6 +23,7 @@ public class RecipeDAOImpl implements RecipeDAO {
 	private static final String SQL_SELECT_TAG_IDS = "SELECT tag_id FROM RecipeTags WHERE recipe_id = ?";
     private static final String SQL_SELECT_LESS_THAN_LEVELS = "SELECT recipe_id, recipe_name, recipe_image_url, recipe_url, image_pic, ingredients_list, portion_calorie, portion_fat, portion_carbo, portion_protein FROM Recipe WHERE portion_calorie BETWEEN 0 AND ? AND portion_fat <= ? AND portion_protein <= ? AND portion_carbo <= ? ORDER BY portion_calorie DESC";
     private static final String SQL_SELECT_LESS_THAN_LEVELS_AND_CATEGORY = "SELECT Recipe.recipe_id, recipe_name, recipe_image_url, recipe_url, image_pic, ingredients_list, portion_calorie, portion_fat, portion_carbo, portion_protein FROM Recipe JOIN RecipeCategories ON RecipeCategories.recipe_id = Recipe.recipe_id JOIN JDFCategory on RecipeCategories.category_id = JDFCategory.category_id WHERE portion_calorie BETWEEN 0 AND ? AND portion_fat <= ? AND portion_protein <= ? AND portion_carbo <= ? AND JDFCategory.category_name = ? ORDER BY portion_calorie DESC";
+    private static final String SQL_SELECT_IDS_BY_CATEGORY = "SELECT Recipe.recipe_id FROM Recipe JOIN RecipeCategories ON RecipeCategories.recipe_id = Recipe.recipe_id JOIN JDFCategory ON JDFCategory.category_id = RecipeCategories.category_id WHERE JDFCategory.category_name = ?";
 
  	private static final String SQL_SELECT_LESS_THAN_LEVELS_ORDER_BY_CB_PREDICTIONS_WITH_LIMIT = 
     "SELECT category_name, CBUserPredictions.prediction, Recipe.recipe_id, Recipe.recipe_name,"+
@@ -167,6 +168,28 @@ public class RecipeDAOImpl implements RecipeDAO {
         }
 
         return recipes;
+    }
+
+    @Override
+    public List<Long> getRecipeIdsByCategory(String categoryName) throws DAOException{
+    	List<Long> ids = new ArrayList<Long>();
+    	Connection connexion = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connexion = daoFactory.getConnection();
+            preparedStatement = initializationPreparedRequest( connexion, SQL_SELECT_IDS_BY_CATEGORY, false, categoryName);
+            resultSet = preparedStatement.executeQuery();
+            while ( resultSet.next() ) {
+                ids.add(resultSet.getLong("recipe_id"));
+            }
+        } catch ( SQLException e ) {
+            throw new DAOException( e );
+        } finally {
+            silentClosures( resultSet, preparedStatement, connexion );
+        }
+
+        return ids;
     }
 
 	private void addSubCategory(Recipe recipe) {
