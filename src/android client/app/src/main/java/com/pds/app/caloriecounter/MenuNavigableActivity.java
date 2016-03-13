f3dd5fbe871d49f6ddfb6addd0a3306d916d82e7
Aurelien van Delft <avdelft@ulb.ac.be>
2016-03-13 23:53:03 +0100

    Deleting settings points

--- src/android client/app/src/main/java/com/pds/app/caloriecounter/MenuNavigableActivity.java
+++ src/android client/app/src/main/java/com/pds/app/caloriecounter/MenuNavigableActivity.java
@@ -62,12 +62,6 @@ public abstract class MenuNavigableActivity extends NotifiableActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -75,11 +69,6 @@ public abstract class MenuNavigableActivity extends NotifiableActivity
         // automatically handle clicks on the Hom/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
